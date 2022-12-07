bfe85d6422d58aad0603b5adce63dfdb0af5d0d9
yakitorii <yuki.torii.23@gmail.com>
2022-12-08 07:07:49 +0900

    WebScoketを使ったブロードキャスト方式に変更

--- hotwire-tw-demo/app/models/tweet.rb
+++ hotwire-tw-demo/app/models/tweet.rb
@@ -3,4 +3,6 @@ class Tweet < ApplicationRecord
   validates :body, presence: true
   validates :icon, presence: true
   enum :icon, { love: 0, angry: 1, smile: 2 }
+
+  after_create_commit -> { broadcast_prepend_to("tweets") }
 end
