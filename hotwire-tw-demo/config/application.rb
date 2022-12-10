93b669701a2b653d81e54d082a0a991834da1137
k.yorimitsu <k.yorimitsu@everyleaf.com>
2022-12-10 09:24:11 +0900

    field_with_errorsクラスを自動挿入させないように修正
    検証エラー時にfield_with_errorsクラスが自動挿入されるためデザイン崩れてしてしまっていました。
    他の箇所でこのクラスは使っていないかと思うので、自動挿入しないよう設定を追加しました。

--- hotwire-tw-demo/config/application.rb
+++ hotwire-tw-demo/config/application.rb
@@ -31,5 +31,7 @@ module HotwireTwDemo
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
